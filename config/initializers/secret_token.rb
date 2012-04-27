42edd764bf3a8dee047119f36d9e1a663f767314
Jeff Dickey <jeff@dickey.xxx>
2012-04-27 12:52:15 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WheelerBoard::Application.config.secret_token = 'e07a6d71f5b9fc66bb9fee898b196b8b5ba2621ac8c99b957e964be91545b642f3e58718cc30fc1e96091dd0faec39fd4b3504b5869ed6b5f7ae367d79e783b7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WheelerBoard::Application.config.session_store :cookie_store, key: '_wheeler_board_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WheelerBoard::Application.config.session_store :active_record_store
