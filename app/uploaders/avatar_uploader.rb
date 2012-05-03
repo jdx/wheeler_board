939ce6ad2783d6bb06470dc91108c08dde10c32d
Jeff Dickey <jeff@dickey.xxx>
2012-05-02 22:41:25 -0700

    fixed image sizes

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -13,7 +13,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fit: [370, 800]
+  process resize_to_fit: [600, 800]
 
   version :thumb do
     process resize_to_fit: [100, 100]
